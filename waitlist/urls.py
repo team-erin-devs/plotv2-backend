4f61ace9af10ee254cfbc1a5414756482624bd6a
sinndu <sindusaram@gmail.com>
2025-10-29 17:21:06 -0400

    no more waitlist/waitlist

--- waitlist/urls.py
+++ waitlist/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import WaitlistCreateView
 
 urlpatterns = [
-    path('waitlist/', WaitlistCreateView.as_view(), name='waitlist-create'),
+    path("", WaitlistCreateView.as_view(), name='waitlist-create'),
 ]
